2c6245951319dbef9837519c017e06567cfd3431
Premkrishna2442 <premkrishna222@gmail.com>
2022-06-14 18:45:33 +0530

    import change link

--- src/components/Import.js
+++ src/components/Import.js
@@ -63,9 +63,7 @@ class Import extends Component {
             placeholder='End time in formate :"2022-04-29T18:00:00+05:30"'
           />
           <button onClick={() => this.insertEvent()} className="Input button">
-            <Link to="/">
-              <p className="buttonText">Import Event</p>
-            </Link>
+           
           </button>
         </div>
       </div>
